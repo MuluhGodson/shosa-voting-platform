b657fd9c8e93104cd6379ed7fec60aa2d1940c1a
Muluh MG Godson <godsonmuluh@gmail.com>
2021-09-09 22:29:26 +0100

    update appsP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(125);
+        Schema::defaultStringLength(255);
     }
 }
