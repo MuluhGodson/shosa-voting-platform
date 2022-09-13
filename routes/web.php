778d38585ef06695fd621154cbe9757919c61c93
Muluh MG Godson <godsonmuluh@gmail.com>
2022-09-13 11:15:43 +0100

    add paymooney success route and remove subdomain

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\VoteController;
 use App\Http\Controllers\ContestController;
 use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\PaymooneyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +18,17 @@ use App\Http\Controllers\CandidateController;
 |
 */
 
-Route::domain('vote.'.config('app.domain'))->group(function () {
-    //Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+//Route::domain('vote.'.config('app.domain'))->group(function () {
     Route::get('/', [VoteController::class, 'index'])->name('welcome');
     Route::get('/apply', [PageController::class, 'apply'])->name('apply'); //If enabling the route below change this route name to simply apply.
-    //Route::get('/apply/{contest}', [PageController::class, 'application'])->name('candidate.apply');
+    Route::get('/apply/{contest}', [PageController::class, 'application'])->name('candidate.apply');
     Route::get('/vote', [VoteController::class, 'index'])->name('vote');
     Route::get('/vote/{contest}', [VoteController::class, 'show'])->name('vote.candidate');
+
+    // Paymooney
+    Route::prefix('paymooney')->group(function () {
+        Route::get('/success', [PaymooneyController::class, 'success']);
+    });
 
     Route::middleware(['web', 'auth:sanctum', 'verified'])->group(function () {
         Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
@@ -33,4 +38,4 @@ Route::domain('vote.'.config('app.domain'))->group(function () {
         Route::get('/payment/request', [VoteController::class, 'paymentRequest'])->name('pay.request');
         Route::get('/finance', [PageController::class, 'finance'])->name('finance.index');
     });
-});
+//});
